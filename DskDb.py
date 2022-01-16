f3b04ea787e661b04a32e034122b87ff697e4092
lomnom <zhaoxiong.ang@gmail.com>
2022-01-16 21:08:11 +0800

    works

--- DskDb.py
+++ DskDb.py
@@ -1,4 +1,3 @@
-# supports ints, str, list and dict
 # headers are \0<type>
 
 #structure is <header|(\0type header|type data)+>
@@ -26,19 +25,24 @@ class StoreSpec:
 		self.types[storable.stored]=storable
 
 	def store(self,data,file,force=False):
-		file.seek(0)
 		header=file.read(len(self.header))
 		if not force and header != self.header:
 			raise NoHeaderWarning
 
 		file.write(self.header)
-		file.write(self.types[type(data)].encode(data)+b"\0\0")
+		file.write(self.encode(data)+b"\0\0")
+
+	def encode(self,data):
+		return self.types[type(data)].encode(data)
 
 	def read(self,file):
 		header=file.read(len(self.header))
 		if header != self.header:
-			raise NoHeaderError
+			raise self.NoHeaderError
 
+		return self.decode(file)
+
+	def decode(self,file):
 		header=file.read(2)
 		return self.headers[header].decode(file)
 
@@ -46,16 +50,15 @@ class Storable:
 	def __init__(self,stored,header):
 		assert(type(header)==bytes)
 		self.stored=stored
-		self.header=header
+		self.headerCh=header
+		self.header=b'\0'+header
 
-	#returns header+data
 	def encoder(self,func):
 		self._encode=func
 
 	def encode(self,file):
 		return self._encode(self,file)
 
-	#returns data+length
 	def decoder(self,func):
 		self._decode=func
 
@@ -64,34 +67,117 @@ class Storable:
 
 diskDb=StoreSpec(b"DiskDb, another shitty database format!")
 
+# bytes <-> int
+def iTb(number):
+	return number.to_bytes(4,"big")
+
+def bTi(data):
+	return int.from_bytes(data,"big")
+
 # Store integers
-storeInt=Storable(int,b'\0i')
+storeInt=Storable(int,b'i')
 diskDb.addStorable(storeInt)
 
 @storeInt.encoder
-def encodeInt(spec,number):
-	return spec.header+number.to_bytes(4,"big")
+def encodeInt(self,number):
+	return self.header+iTb(number)
 
 @storeInt.decoder
-def decodeInt(spec,file):
-	return int.from_bytes(file.read(4),"big")
+def decodeInt(self,file):
+	return bTi(file.read(4))
 
 # Strings
-storeStr=Storable(str,b'\0s')
+storeStr=Storable(str,b's')
 diskDb.addStorable(storeStr)
 
 @storeStr.encoder
-def encodeStr(spec,text):
+def encodeStr(self,text):
 	strData=bytes(text.encode("UTF8"))
-	header=spec.header+len(strData).to_bytes(4,"big")
+	header=self.header+iTb(len(strData))
 	return header+strData
 
 @storeStr.decoder
-def decodeStr(spec,file):
-	length=int.from_bytes(file.read(4),"big")
+def decodeStr(self,file):
+	length=bTi(file.read(4))
 	strData=str(file.read(length),encoding="UTF8")
 	return strData
 
+# NoneType
+storeNone=Storable(type(None),b'n')
+diskDb.addStorable(storeNone)
+
+@storeNone.encoder
+def encodeNone(self,data):
+	return self.header
+
+@storeNone.decoder
+def decodeNone(self,file):
+	return None
+
+# Boolean
+storeBool=Storable(bool,b'b')
+diskDb.addStorable(storeBool)
+
+@storeBool.encoder
+def encodeBool(self,data):
+	return self.header+ (b'\1' if data else b'\0')
+
+@storeBool.decoder
+def decodeBool(self,file):
+	return bool(file.read(1)[0])
+
 # Lists 
-storeList=Storable(str,b'\0l')
+storeList=Storable(list,b'l')
 diskDb.addStorable(storeList)
+
+@storeList.encoder
+def encodeList(self,data):
+	header=self.header+iTb(len(data))
+	encodedData=b''
+	for item in data:
+		encodedData+=self.spec.encode(item)
+	return header+encodedData
+
+@storeList.decoder
+def decodeList(self,file):
+	length=bTi(file.read(4))
+	result=[]
+	for item in range(length):
+		result+=[self.spec.decode(file)]
+	return result
+
+# Dictionaries
+storeDict=Storable(dict,b'd')
+diskDb.addStorable(storeDict)
+
+@storeDict.encoder
+def encodeDict(self,data):
+	length=iTb(len(data))
+	encodedData=b''
+	for key in data:
+		encodedData+=self.spec.encode(key)+self.spec.encode(data[key])
+	return self.header+length+encodedData
+
+@storeDict.decoder
+def decodeDict(self,file):
+	length=bTi(file.read(4))
+	result={}
+	for item in range(length):
+		key=self.spec.decode(file)
+		value=self.spec.decode(file)
+		result[key]=value
+	return result
+
+# Bytes
+storeBytes=Storable(bytes,b'B')
+diskDb.addStorable(storeBytes)
+
+@storeBytes.encoder
+def encodeBytes(self,data):
+	header=self.header+iTb(len(data))
+	return header+data
+
+@storeBytes.decoder
+def decodeBytes(self,file):
+	length=bTi(file.read(4))
+	return file.read(length)
--- Store.py
+++
@@ -1,7 +0,0 @@
-from DskDb import *
-from io import BytesIO
-
-db=open("db.dskdb","w+b")
-
-diskDb.store("hMM",db,force=True)
-print(diskDb.read(db))
